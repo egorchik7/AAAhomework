1c3ce41fe8a8a9559cbb01adba2cb2cbff49a4a7
Egor <90351343+egorchik7@users.noreply.github.com>
2021-12-13 19:19:23 +0300

    Create Test_HomeWork6_3.py

---
+++ HomeWork6_test/test3/Test_HomeWork6_3.py
@@ -0,0 +1,74 @@
+from typing import List, Tuple
+import unittest
+
+
+def fit_transform(*args: str) -> List[Tuple[str, List[int]]]:
+    """
+    fit_transform(iterable)
+    fit_transform(arg1, arg2, *args)
+    """
+    if len(args) == 0:
+        raise TypeError('expected at least 1 arguments, got 0')
+
+    categories = args if isinstance(args[0], str) else list(args[0])
+    uniq_categories = set(categories)
+    bin_format = f'{{0:0{len(uniq_categories)}b}}'
+
+    seen_categories = dict()
+    transformed_rows = []
+
+    for cat in categories:
+        bin_view_cat = (int(b) for b in bin_format.format(1 << len(seen_categories)))
+        seen_categories.setdefault(cat, list(bin_view_cat))
+        transformed_rows.append((cat, seen_categories[cat]))
+
+    return transformed_rows
+
+
+class TestTF(unittest.TestCase):
+    def test_tf(self):
+        actual = fit_transform('Moscow', 'New York', 'Moscow', 'London')
+        expected = [('Moscow', [0, 0, 1]), ('New York', [0, 1, 0]),
+                    ('Moscow', [0, 0, 1]), ('London', [1, 0, 0])]
+
+        self.assertEqual(actual, expected)
+
+    def test_empty(self):
+        actual = list(fit_transform(''))
+        expected = [('', [1])]
+
+        self.assertEqual(actual, expected)
+
+    def test_in(self):
+        actual = fit_transform('Moscow', 'New York', 'Moscow', 'London')
+        expected = ('London', [1, 0, 0])
+
+        self.assertIn(expected, actual)
+
+    def test_not_in(self):
+        actual = fit_transform('Moscow', 'New York', 'Moscow', 'London')
+        expected = ('London', [1, 1, 1])
+
+        self.assertNotIn(expected, actual)
+
+    def test_exception(self):
+        actual = fit_transform('Moscow', 'New York', 'Moscow', 'London')
+        expected = 'Rome'
+
+        self.assertRaises(TypeError, actual, expected)
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+
+    cities = ['Moscow', 'New York', 'Moscow', 'London']
+    exp_transformed_cities = [
+        ('Moscow', [0, 0, 1]),
+        ('New York', [0, 1, 0]),
+        ('Moscow', [0, 0, 1]),
+        ('London', [1, 0, 0]),
+    ]
+    transformed_cities = fit_transform(cities)
+    pprint(transformed_cities)
+    assert transformed_cities == exp_transformed_cities
+    unittest.main()
