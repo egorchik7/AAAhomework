5465e58fbafef6b19d8ee2fafbf5c59d93fca39b
Egor <90351343+egorchik7@users.noreply.github.com>
2021-12-13 19:11:59 +0300

    Create HomeWork6_2_test.py

---
+++ HomeWork6_test/test2/HomeWork6_2_test.py
@@ -0,0 +1,50 @@
+import pytest
+LETTER_TO_MORSE = {
+    'A': '.-', 'B': '-...', 'C': '-.-.',
+    'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..',
+    'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-',
+    'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '1': '.----',
+    '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.', '0': '-----',
+    ', ': '--..--', '.': '.-.-.-', '?': '..--..',
+    '/': '-..-.', '-': '-....-', '(': '-.--.', ')': '-.--.-',
+    ' ': ' '
+}
+
+MORSE_TO_LETTER = {
+    morse: letter
+    for letter, morse in LETTER_TO_MORSE.items()
+}
+
+
+def decode(morse_message: str) -> str:
+    """
+    Декодирует строку из азбуки Морзе в английский
+    """
+    decoded_letters = [
+        MORSE_TO_LETTER[letter] for letter in morse_message.split()
+    ]
+
+    return ''.join(decoded_letters)
+
+
+@pytest.mark.parametrize('s,exp', [
+   ('.-', 'A'),
+   ('-...', 'B'),
+   ('-.-.', 'C'),
+   ('-- .- ..', 'MAI')
+])
+def test_decode(s, exp):
+    assert decode(s) == exp
+
+
+if __name__ == '__main__':
+    morse_msg = '-- .- .. -....- .--. -.-- - .... --- -. -....- ..--- ----- .---- ----.'
+    decoded_msg = decode(morse_msg)
+    print(decoded_msg)
